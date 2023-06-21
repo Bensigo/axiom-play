406937e349cb61083c73f58cd2d39de2652609d5
bensigo <sigocode@gmail.com>
2023-06-21 23:29:24 +0400

    fix build

--- components/charts/Bar.tsx
+++ components/charts/Bar.tsx
@@ -10,74 +10,69 @@ import { ChartOptions } from "chart.js";
 
 const BarChart: React.FC<{ data: any }> = ({ data }) => {
 
-  const options: ChartOptions = {
-    responsive: true,
-    normalized: true,
-    // scales: {
-    //   // x: {
-    //   //   offset: false,
-    //   //   stacked: true,
-    //   //   type: "timeseries",
-    //   //   time: {
-    //   //     unit: "hour",
-    //   //   },
+  // const options: ChartOptions = {
+  //   responsive: true,
+  //   normalized: true,
+  //   scales: {
+  //     x: {
+  //       // offset: false,
+  //       stacked: true,
+  //       // type: "timeseries",
+  //       // time: {
+  //       //   unit: "hour",
+  //       // },
 
-    //   //   grid: {
-    //   //     offset: false,
-    //   //     tickColor: "rgba(75, 192,, 192, 1)",
-    //   //     color: "rgba(75, 192,, 192, 1)",
-    //   //   },
-    //   //   ticks: {
-    //   //     stepSize: 3,
-    //   //     font: {
-    //   //       size: 13,
-    //   //     },
-    //   //   },
-    //   // },
-    //   // y: {
+  //       // grid: {
+  //       //   offset: false,
+  //       //   tickColor: "rgba(75, 192,, 192, 1)",
+  //       //   color: "rgba(75, 192,, 192, 1)",
+  //       // },
+  //       // ticks: {
+  //       //   stepSize: 3
+  //       // },
+  //     },
+  //     y: {
       
-    //   //   stacked: true,
-    //   //   type: 'linear',
-    //   //   ticks: {
-    //   //     sampleSize: 10,
-    //   //     // callback: function(val, index) {
-    //   //     //     // Hide the label of every 2nd dataset
-    //   //     //     return index % 2 === 0 ? this.getLabelForValue(val as number) : '';
-    //   //     //   },
-    //   //     font: {
-    //   //       size: 13,
-    //   //     },
-    //   //   },
-    //   // },
-    // },
-    animation: false,
-    plugins: {
-      autocolors: {
-        enabled: true,
-      },   
-      legend: {
-        display: false,
-      },
-      // tooltip: {
-      //     itemSort: function (itemA, itemB) {
-      //         // Sort items in descending order based on their value
-      //         return itemB.parsed.y - itemA.parsed.y;
-      //       }
-      // }
-    },
-    elements: {
-      point: {
-        radius: 0,
-      },
-    },
-  };
+  //       stacked: true,
+  //       // type: 'linear',
+  //       // ticks: {
+  //       //   sampleSize: 10,
+         
+  //       //   font: {
+  //       //     size: 13,
+  //       //   },
+  //       // },
+  //     },
+  //   },
+  //   animation: false,
+  //   plugins: {
+  //     autocolors: {
+  //       enabled: true,
+  //     },   
+  //     legend: {
+  //       display: false,
+  //     },
+  //     // tooltip: {
+  //     //     itemSort: function (itemA, itemB) {
+  //     //         // Sort items in descending order based on their value
+  //     //         return itemB.parsed.y - itemA.parsed.y;
+  //     //       }
+  //     // }
+  //   },
+  //   elements: {
+  //     point: {
+  //       radius: 0,
+  //     },
+  //   },
+  // };
 
-  if (!data)return null;
-  return (
-    <>
-      <Bar options={options} data={data} />
-    </>
-  );
+  // if (!data)return null;
+  // return (
+  //   <>
+  //     <Bar options={options} data={data} />
+  //   </>
+  // );
+  return <></>
 };
 
 export default BarChart
--- components/charts/Line.tsx
+++ components/charts/Line.tsx
@@ -3,70 +3,71 @@ import { ChartOptions } from "chart.js";
 import { Line } from "react-chartjs-2";
 
 const LineChart: React.FC = ({ data }: any) => {
-  const options: ChartOptions = {
-    responsive: true,
-    //    parsing: false,
-    normalized: true,
-    scales: {
-      x: {
-        offset: false,
-        type: "timeseries",
-        time: {
-          unit: "hour",
-        },
+  // const options: ChartOptions = {
+  //   responsive: true,
+  //   //    parsing: false,
+  //   normalized: true,
+  //   scales: {
+  //     x: {
+  //       offset: false,
+  //       type: "timeseries",
+  //       time: {
+  //         unit: "hour",
+  //       },
 
-        grid: {
-          offset: false,
-          tickColor: "rgba(75, 192,, 192, 1)",
-          color: "rgba(75, 192,, 192, 1)",
-        },
-        ticks: {
-          stepSize: 3,
-          font: {
-            size: 13,
-          },
-        },
-      },
-      y: {
-        ticks: {
-          sampleSize: 20,
-          font: {
-            size: 13,
-          },
-        },
-      },
-    },
-    animation: false,
-    plugins: {
-      autocolors: {
-        enabled: true,
-      },
-      legend: {
-        display: false,
-      },
-      // tooltip: {
-      //     itemSort: function (itemA, itemB) {
-      //         // Sort items in descending order based on their value
-      //         return itemB.parsed.y - itemA.parsed.y;
-      //       }
-      // }
-    },
-    elements: {
-      point: {
-        radius: 0,
-      },
-    },
-  };
+  //       grid: {
+  //         offset: false,
+  //         tickColor: "rgba(75, 192,, 192, 1)",
+  //         color: "rgba(75, 192,, 192, 1)",
+  //       },
+  //       ticks: {
+  //         stepSize: 3,
+  //         font: {
+  //           size: 13,
+  //         },
+  //       },
+  //     },
+  //     y: {
+  //       ticks: {
+  //         sampleSize: 20,
+  //         font: {
+  //           size: 13,
+  //         },
+  //       },
+  //     },
+  //   },
+  //   animation: false,
+  //   plugins: {
+  //     autocolors: {
+  //       enabled: true,
+  //     },
+  //     legend: {
+  //       display: false,
+  //     },
+  //     // tooltip: {
+  //     //     itemSort: function (itemA, itemB) {
+  //     //         // Sort items in descending order based on their value
+  //     //         return itemB.parsed.y - itemA.parsed.y;
+  //     //       }
+  //     // }
+  //   },
+  //   elements: {
+  //     point: {
+  //       radius: 0,
+  //     },
+  //   },
+  // };
 
-  if (!data) {
-    return null;
-  }
+  // if (!data) {
+  //   return null;
+  // }
 
-  return (
-    <>
-      <Line options={options} data={data} />
-    </>
-  );
+  // return (
+  //   <>
+  //     <Line options={options} data={data} />
+  //   </>
+  // );
+  return <></>
 };
 
 export default LineChart;
